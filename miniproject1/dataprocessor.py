55df0b040fabf70e7b187cb2328a8052358fe0d2
16061025 <39218055+16061025@users.noreply.github.com>
2022-03-25 11:28:59 +0800

    Update dataprocessor.py

--- miniproject1/dataprocessor.py
+++ miniproject1/dataprocessor.py
@@ -12,7 +12,7 @@ import aug
 
 class DataProcessor:
 
-    def __init__(self):
+    def __init__(self, aug=False):
 
         data_ROOT = netconfig.data_ROOT
 
@@ -25,15 +25,15 @@ class DataProcessor:
                                      download=False,
                                      transform=data_transforms)
 
-        self.train_data = datasets.CIFAR10(root=data_ROOT,
+       if(aug):
+            self.train_data = aug.augData()
+        else:
+            self.train_data = datasets.CIFAR10(root=data_ROOT,
                                       train=True,
                                       download=False,
                                       transform=data_transforms)
 
         batch_size = netconfig.batch_size
 
-        self.aug_train_data = aug.augData()
         self.train_dataloader = DataLoader(self.train_data, shuffle=True, batch_size=batch_size)
-        self.train_aug_dataloader = DataLoader(self.aug_train_data, shuffle=True, batch_size=batch_size)
         self.test_dataloader = DataLoader(self.test_data, batch_size=batch_size)
-
